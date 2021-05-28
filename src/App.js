c7e5a3b33da5bf9a84b1dbf9c4ab532a8075de46
deepak sharma <deepak.sharma>
2021-05-28 23:13:44 +0530

    all inital work

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Changes ww To React App and edit post is there to have seen
+       </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
